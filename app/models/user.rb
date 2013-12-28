52c7b033bb5dd5d876aea21393d692cec14f84a5
Peter Luptak <dabavil@gmail.com>
2013-12-28 13:44:19 -0500

    Hello! This is another commit, after we've implemented the auth feature of el Devisio. Seems to work fine...except for a few glitches and really uggly stylez.
    wq

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,22 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,
   					:first_name, :last_name, :profile_name
+
+  has_many :statuses
+
+    def full_name
+      first_name + " " + last_name
+    end
+
 end
+
+
